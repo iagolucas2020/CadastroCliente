ecd907c043dfdfac60b53cecb78754c8a6503096
Iago <iago0905@hotmail.com>
2024-04-27 08:16:45 -0300

    Implementação gerador de pdf clientes

--- src/Pages/Clientes/index.js
+++ src/Pages/Clientes/index.js
@@ -1,12 +1,10 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./Clientes.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ModalAdd from "../../components/Clientes/modalAdd";
 import { AlertBasic, AlertConfirm } from "../../components/Alert";
-import { get, remove } from "../../services/clientes";
+import { get, getFilter, getPdf, remove } from "../../services/clientes";
 import ModalEdit from "../../components/Clientes/modalEdit";
-
-
 
 function Clientes() {
   const [data, setData] = useState([]);
@@ -14,6 +12,12 @@ function Clientes() {
 
   const [visibleModal, setVisibleModal] = useState(false);
   const [visibleEditar, setVisibleEditar] = useState(false);
+
+  //Dados Filtro
+  const [id, setId] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     getData();
@@ -37,14 +41,23 @@ function Clientes() {
   };
 
   const removeCliente = async (id) => {
-    const response = await AlertConfirm('Exclusão', 'Tem certeza que deseja excluir este cliente, id: ' + id + ".", 'question')
-    if(response.isConfirmed){
+    const response = await AlertConfirm(
+      "Exclusão",
+      "Tem certeza que deseja excluir este cliente, id: " + id + ".",
+      "question"
+    );
+    if (response.isConfirmed) {
       const result = await remove(id);
       if (result.status === 200) {
         getData();
         AlertBasic("Exclusão", "Cliente excluído com sucesso.", "success");
       }
-    };
+    }
+  };
+
+  const filter = async () => {
+    const response = await getFilter(id, cpf, nome, email);
+    setData(response.data);
   };
 
   return (
@@ -61,8 +74,66 @@ function Clientes() {
           >
             Novo
           </button>
+          <button
+            className="btn btn-dark"
+            onClick={() => {
+              getPdf();
+            }}
+          >
+            Download
+          </button>
+          <button
+            className="btn btn-warning"
+            onClick={() => {
+              filter();
+            }}
+          >
+            Pesquisar
+          </button>
         </div>
       </header>
+      <br />
+      <div className="panel-body">
+        <div className="row">
+          <div className="form-group col-sm-2">
+            <label>ID</label>
+            <input
+              className="form-control"
+              type="number"
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+            />
+          </div>
+          <div className="form-group col-sm-3">
+            <label>CPF</label>
+            <input
+              className="form-control"
+              type="number"
+              value={cpf}
+              onChange={(e) => setCpf(e.target.value)}
+            />
+          </div>
+          <div className="form-group col-sm-3">
+            <label>NOME</label>
+            <input
+              className="form-control"
+              type="text"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+            />
+          </div>
+          <div className="form-group col-sm-4">
+            <label>E-MAIL</label>
+            <input
+              className="form-control"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
+      <br />
       <table className="table table-bordered table-striped table-hover">
         <thead>
           <tr>
--- src/services/clientes.js
+++ src/services/clientes.js
@@ -1,34 +1,58 @@
-import { api } from './api';
+import axios from "axios";
+import { api, linkApi } from "./api";
 
-//Get
+//GET
 async function get() {
-  return await api.get('/Clientes');
+  return await api.get("/Clientes");
+}
+
+//GET FILTER
+async function getFilter(id, cpf, nome, email) {
+  const response = await api.get("/Clientes/filtro", {
+    params: {
+      id,
+      cpf,
+      nome,
+      email,
+    },
+  });
+  return response;
 }
 
 //POST
 async function post(clientes) {
-  const response = await api.post(
-    '/Clientes',
-    clientes,
-  );
+  const response = await api.post("/Clientes", clientes);
   return response;
 }
 
 //PUT
 async function put(id, clientes) {
-  const response = await api.put(
-    '/Clientes/'+ id,
-    clientes,
-  );
+  const response = await api.put("/Clientes/" + id, clientes);
   return response;
 }
 
 //Delete
 async function remove(id) {
-  const response = await api.delete(
-    '/Clientes/' + id,
-  );
+  const response = await api.delete("/Clientes/" + id);
   return response;
 }
 
-export { get, post, put, remove };
+//GetPdf
+async function getPdf() {
+  const data = {};
+  return axios({
+    url: linkApi.link + "/Clientes/pdf",
+    method: "GET",
+    responseType: "blob",
+    params: data,
+  }).then((response) => {
+    const url = window.URL.createObjectURL(new Blob([response.data]));
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", "relatorio.pdf");
+    document.body.appendChild(link);
+    link.click();
+  });
+}
+
+export { get, getFilter, post, put, remove, getPdf };
